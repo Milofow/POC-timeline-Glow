fce0b7ee88110b10c4261377d70cd31a88831c11
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2022-06-21 11:50:53 +0200

    Added POC

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+const { io } = require('socket.io-client');
+const socket = io.connect("https://glow-websocket-server.herokuapp.com");
+
+
+function App() {
+
+  const [gyroY, setGyroY] = useState(0.0);
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    socket.on("timeCallback", (data) => {
+      setGyroY(data);
+      //console.log(gyroY);
+      setTime(time + gyroY);
+      console.log(time);
+    })
+  })
+
+
+
+  return (
+    <div className="App">
+      {time < -1.2? <img src={require('./images/eindhoven-station-1866.jpg')} ></img> : <h1></h1> }
+      {time > -1.2 && time < 1.2? <img src={require('./images/eindhoven-station-1916.jpg')} ></img> : <h1></h1> }
+      {time > 1.2? <img src={require('./images/eindhoven-station-1955.jpg')} ></img> : <h1></h1> }
+    </div>
+  );
+}
+
+export default App;
