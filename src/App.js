1a198826d97123eaac0837508b841c6c896eeb49
Timmermans,Milo M.P.A <milo.timmermans@student.fontys.nl>
2022-06-21 11:57:41 +0200

    removed warnings

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ function App() {
 
   return (
     <div className="App">
-      {time < -1.2? <img src={require('./images/eindhoven-station-1866.jpg')} ></img> : <h1></h1> }
-      {time > -1.2 && time < 1.2? <img src={require('./images/eindhoven-station-1916.jpg')} ></img> : <h1></h1> }
-      {time > 1.2? <img src={require('./images/eindhoven-station-1955.jpg')} ></img> : <h1></h1> }
+      {time < -1.2? <img src={require('./images/eindhoven-station-1866.jpg')} alt="eindhoven-station" ></img> : <></> }
+      {time > -1.2 && time < 1.2? <img src={require('./images/eindhoven-station-1916.jpg')} alt="eindhoven-station" ></img> : <></> }
+      {time > 1.2? <img src={require('./images/eindhoven-station-1955.jpg')} alt="eindhoven-station" ></img> : <></> }
     </div>
   );
 }
